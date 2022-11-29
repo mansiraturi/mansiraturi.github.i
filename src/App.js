2f798f92c56690b64f63d067d288657c28878757
Mansi Raturi <mansi.r0211@gmail.com>
2022-11-29 23:35:49 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import KnowMe from "./KnowMe";
+import Project from "./Project";
+import Contact from "./Contact";
+import Resume from "./Resume";
+import Slider from "./Slider";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />      
+      <KnowMe />
+      <Project/>
+      <Slider/>
+      <Resume/>
+      <Contact/>
+      
+    </div>
+  );
+
+}
+
+
+export default App;
